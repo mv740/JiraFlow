cdb787b1fde23bd29b9130e670c916f801a2ba77
michal <michalwozniak@live.ca>
2016-04-30 16:34:55 -0400

    Swipe view design for project activity

--- JiraFlow/app/src/main/java/ca/michalwozniak/jiraflow/ProjectActivity.java
+++ JiraFlow/app/src/main/java/ca/michalwozniak/jiraflow/ProjectActivity.java
@@ -1,17 +1,31 @@
 package ca.michalwozniak.jiraflow;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import ca.michalwozniak.jiraflow.adapter.ViewPagerAdapter;
+import ca.michalwozniak.jiraflow.fragment.One;
+import ca.michalwozniak.jiraflow.fragment.Two;
+
 public class ProjectActivity extends AppCompatActivity {
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.tabs)
+    TabLayout tabLayout;
+    @Bind(R.id.viewpager)
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         if(toolbar!=null)
         {
             setSupportActionBar(toolbar);
@@ -27,7 +41,16 @@ public class ProjectActivity extends AppCompatActivity {
                 }
             });
         }
+        
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
 
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new One(), "ONE");
+        adapter.addFragment(new Two(), "TWO");
+        viewPager.setAdapter(adapter);
     }
 
 }
